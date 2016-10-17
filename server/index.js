c03b12abc066d9ec14012281115c28e99649da9f
Harm <harm@bit-students.com>
2016-10-17 20:55:41 +0200

    Added template files

--- server/index.js
+++ server/index.js
@@ -1,10 +1,17 @@
 import express from 'express';
+import { resolve } from 'path';
+
 
 const app = express();
 const host = process.env.HOST || '0.0.0.0';
 const port = process.env.PORT || '1337';
 
-app.get('/', (req, res) => res.end('Hell, no Kitty!'));
+
+
+
+app.use(express.static( resolve(__dirname, '..','public')));
+
+// app.get('/', (req, res) => res.end('Hell, no Kitty!'));
 
 app.listen(port, host, (err) => {
 	if (err) console.log('Shit gaat slecht bruh!');
